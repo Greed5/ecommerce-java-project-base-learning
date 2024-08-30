6ff40fa9b2483d2c0824b7dbf534f3df42526300
Greedy5 <theonlysela333@gmail.com>
2024-08-30 22:39:12 +0700

    Update Market.java
    made some change to Error with Market view

--- src/forms/Market.java
+++ src/forms/Market.java
@@ -5,25 +5,48 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Market extends JDialog{
-    private JPanel HeadPanel;
-    private JPanel ProductPane;
-    private JList product_List;
+public class Market extends JDialog {
+
+    private JTable table1;
     private JButton marketButton;
     private JButton cartOrderButton;
     private JButton accountButton;
     private JPanel MainPanel;
 
-    public Market(Market Frame) {
+    public Market(JFrame parent) {
         setContentPane(MainPanel);
         setTitle("Market");
         setMinimumSize(new Dimension(1024, 768));
         setModal(true);
-        setLocationRelativeTo(Frame);
+        setLocationRelativeTo(parent);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-    }
 
-    private void createUIComponents() {
+        // ActionListener for the Market button
+        marketButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();  // Close the MainForm
+                Market markets = new Market(parent);  // Pass the parent frame
+                markets.setVisible(true);  // Open the Market dialog
+            }
+        });
 
+        cartOrderButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                OrderCart orderCart = new OrderCart(null);
+                orderCart.setVisible(true);
+            }
+        });
+        accountButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                account Account = new account(null);
+                Account.setVisible(true);
+
+            }
+        });
     }
 }
