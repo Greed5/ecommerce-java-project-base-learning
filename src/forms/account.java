e54016aa34d815bf02211e5877ec4f7dfc3568a9
Greedy5 <theonlysela333@gmail.com>
2024-09-01 13:24:06 +0700

    Update account.java
    add button listener market, acc, cart

--- src/forms/account.java
+++ src/forms/account.java
@@ -6,11 +6,13 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class account extends JDialog {
-    private JLabel logolbl;
     private JPanel legpanel;
     private JPanel headPane;
     private JPanel Mainpane;
     private JButton closeButton;
+    private JButton marketButton;
+    private JButton cartOrderButton;
+    private JButton accountButton;
 
     public account(JFrame parent) {
             setContentPane(Mainpane);
@@ -23,9 +25,36 @@ public class account extends JDialog {
         @Override
         public void actionPerformed(ActionEvent e) {
             dispose();
-            Market market = new Market(null);
-            market.setVisible(true);
+            Market_Backup markets = new Market_Backup(null);
+            markets.setVisible(true);
         }
+        });
+        // ActionListener for the Market button
+        marketButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();  // Close the MainForm
+                Market markets = new Market(parent);  // Pass the parent frame
+                markets.setVisible(true);  // Open the Market dialog
+            }
+        });
+
+        cartOrderButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                OrderCart orderCart = new OrderCart(null);
+                orderCart.setVisible(true);
+            }
+        });
+        accountButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                account Account = new account(null);
+                Account.setVisible(true);
+
+            }
         });
     }
 }
